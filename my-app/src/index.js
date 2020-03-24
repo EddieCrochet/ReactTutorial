f8f3988ae8694c6c0891875e4634d2dc72ee6188
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-03-23 22:28:48 -0500

    App does the same thing, but I have made the Square components controlled by the board because their value is passed down to them by the state of the board as opposed to them handling state themselves

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-  {/*have to call super when defining the constructor of a subclass*/}
-    super(props);
-    {/*passes props to the parent constructor*/}
-    this.state = {
-      value: null,
-    };
-  }
+ //constructor(props) {
+ // {/*have to call super when defining the constructor of a subclass*/}
+    //super(props);
+   // {/*passes props to the parent constructor*/}
+   // this.state = {
+     // value: null,
+   // };
+  //}
+  //No constructor needed when Square doesn't keep track of the game's state
 
     render() {
       return (
         <button className="square" 
-onClick={() => this.setState({value: 'X'})}>
+onClick={() => this.props.onClick({value: 'X'})}>
   {/* calling setState from an onClick handler in the render method 
   tells React to re-render the component called from whenever the 
   button is clicked --- when you call setState in a component - 
@@ -23,15 +24,38 @@ onClick={() => this.setState({value: 'X'})}>
   {/*Forgetting () => and writing onClick={alert('click')} 
   is a common mistake, and would fire the alert every 
   time the component re-renders.*/}
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+      /*  Add a constructor to the Board and set the Board’s initial state 
+      to contain an array of 9 nulls corresponding to the 9 squares:  */
+        squares: Array(9).fill(null),
+      };
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      //creating a copy of the squares array to modify instead of modifying the existing array
+      squares[i] = 'X';
+      //state is stored in Board component instead of the individual Square
+      //When the Board's state changes, the Square components re-render auto
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+          <Square 
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)} 
+          />
+        );
     }
   
     render() {
